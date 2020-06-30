4297ee985e5fbc6e136158d5246c16e061ae4686
lmayegun <lmayegun@yahoo.com>
2020-06-30 11:08:55 +0100

    install body-parser and include controllers folder

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 const articles = require('./routes/articles-routes');
 
+app.use(bodyParser.json());
 app.use('/api/articles', articles);
 
 app.listen(port);
--- routes/articles-routes.js
+++ routes/articles-routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/news', (req, res, next)=>{
-  res.send('news articles path :)');
+router.get('/:id', (req, res, next)=>{
+  res.json({message:'news articles path :)'});
 });
 
-router.get('/knowledge', (req, res, next)=>{
-  res.send('knowledge articles path :)');
+router.get('/:cat', (req, res, next)=>{
+  res.json('news articles path :)');
+});
+
+router.post('/', (req, res, next)=>{
+  res.json('post an article');
+});
+
+router.delete('/:id', (req, res, next)=>{
+  res.json({id:`delete post ${req.body.id}`});
 });
 
 module.exports = router;
