f8327b5df6e8804e9c86598489759a9ad691f5cd
lmayegun <lmayegun@yahoo.com>
2020-07-06 20:48:25 +0100

    change app listening port to heroku app server

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-7s3y4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
-    app.listen(port);
+    app.listen( process.env.PORT || port);
   })
   .catch(err => {
     console.log(err);
